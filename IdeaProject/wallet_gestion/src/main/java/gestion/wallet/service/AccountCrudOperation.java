9d443bf3880fcb096b8d573c7d78ae94e61c4c9a
fanambynana <hei.fanambinana@gmail.com>
2023-12-04 15:09:43 +0300

    feat: implement CrudOperation account

--- IdeaProject/wallet_gestion/src/main/java/gestion/wallet/service/AccountCrudOperation.java
+++ IdeaProject/wallet_gestion/src/main/java/gestion/wallet/service/AccountCrudOperation.java
@@ -1,2 +1,27 @@
-package gestion.wallet.service;public class AccountCrudOperation {
+package gestion.wallet.service;
+
+import gestion.wallet.model.Account;
+import gestion.wallet.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountCrudOperation implements CrudOperation<Account>{
+    AccountRepository repository;
+
+    @Override
+    public List<Account> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public List<Account> saveAll(List<Account> toSave) {
+        return repository.saveAll(toSave);
+    }
+
+    @Override
+    public Account save(Account toSave) {
+        return repository.save(toSave);
+    }
 }
