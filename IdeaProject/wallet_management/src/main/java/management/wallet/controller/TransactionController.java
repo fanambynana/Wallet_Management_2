ada99ea51f97de79fb88fdd1509c3da62407fb90
fanambynana <hei.fanambinana@gmail.com>
2023-12-04 17:10:57 +0300

    feat: create transaction controller

--- IdeaProject/wallet_management/src/main/java/management/wallet/controller/TransactionController.java
+++ IdeaProject/wallet_management/src/main/java/management/wallet/controller/TransactionController.java
@@ -1,4 +1,31 @@
 package management.wallet.controller;
 
+import management.wallet.model.Account;
+import management.wallet.model.Transaction;
+import management.wallet.service.TransactionCrudOperation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class TransactionController {
+    TransactionCrudOperation service;
+
+    @GetMapping("/transactions")
+    public List<Transaction> findAll() {
+        return service.findAll();
+    }
+    @PutMapping("/transactions")
+    public List<Transaction> saveAll(List<Transaction> toSave) {
+        return service.saveAll(toSave);
+    }
+    @PutMapping("/transactions")
+    public Transaction save(Transaction toSave) {
+        return service.save(toSave);
+    }
+    @GetMapping("/transactions/{id}")
+    public Transaction findById(@PathVariable int id) {
+        return service.findById(id);
+    }
 }
