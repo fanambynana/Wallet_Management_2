a79f5099926fc7dc87d8c61eda9da74364f85996
Faniry <faniryrandria08@gmail.com>
2023-12-08 10:25:33 +0300

    fix: change fonction

--- IdeaProject/wallet_management/src/main/java/management/wallet/repository/VerificationSelect.java
+++ IdeaProject/wallet_management/src/main/java/management/wallet/repository/VerificationSelect.java
@@ -2,13 +2,12 @@ package management.wallet.repository;
 
 import management.wallet.dbConnection.DbConnect;
 import management.wallet.model.Account;
+import management.wallet.model.AccountName;
+import management.wallet.model.AccountType;
 import management.wallet.model.Transaction;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 @Repository
 public class VerificationSelect {
@@ -16,51 +15,32 @@ public class VerificationSelect {
     Connection connection = dbConnect.createConnection();
     public Account verifyAccountById(int id) {
         try {
-            String query = " SELECT * FROM account WHERE id = " + id;
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
+            String query = " SELECT * FROM account WHERE id = ? ";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, Integer.parseInt("id"));
+            ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
                 return new Account(
                         resultSet.getInt("id"),
-                        resultSet.getString("username"),
-                        resultSet.getString("curency"),
-                        resultSet.getDouble("balance")
+                        (AccountName) resultSet.getObject("account_name"),
+                        resultSet.getBigDecimal("balance_amount"),
+                        resultSet.getTimestamp("balanceUpdateDateTime").toLocalDateTime(),
+                        resultSet.getInt("currencyId"),
+                        (AccountType) resultSet.getObject("account_type")
                 );
             }
-            statement.close();
-            resultSet.close();
         } catch (SQLException sqlException) {
             System.out.println("Verification error :\n" + sqlException.getMessage());
-        }
-        return null;
-    }
-    public Account verifyAccountByUsername(String username) {
-        try {
-            String query = " SELECT * FROM account WHERE username = " + username;
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-
-            if (resultSet.next()) {
-                return new Account(
-                        resultSet.getInt("id"),
-                        resultSet.getString("username"),
-                        resultSet.getString("curency"),
-                        resultSet.getDouble("balance")
-                );
-            }
-            statement.close();
-            resultSet.close();
-        } catch (SQLException exception) {
-            System.out.println("Verification error :\n" + exception.getMessage());
         }
         return null;
     }
 
     public Transaction verifyTransactionById(int id) {
         try {
-            String query = " SELECT * FROM transaction WHERE id = " + id;
-            Statement statement = connection.createStatement();
+            String query = " SELECT * FROM transaction WHERE id = ? ";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, "id");
             ResultSet resultSet = statement.executeQuery(query);
 
             if (resultSet.next()) {
@@ -78,3 +58,4 @@ public class VerificationSelect {
         return null;
     }
 }
+
