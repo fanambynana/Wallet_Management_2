2ffe898a8e0befac9390eed33cd5d858ec12787f
fanambynana <hei.fanambinana@gmail.com>
2023-12-13 21:33:57 +0300

    feat: create method to get current balance by account id

--- IdeaProject/wallet_management/src/main/java/management/wallet/service/BalanceService.java
+++ IdeaProject/wallet_management/src/main/java/management/wallet/service/BalanceService.java
@@ -1,5 +1,6 @@
 package management.wallet.service;
 
+import management.wallet.DAO.AccountDAO;
 import management.wallet.DAO.BalanceDAO;
 import management.wallet.model.Balance;
 import org.springframework.stereotype.Service;
@@ -9,8 +10,14 @@ import java.time.LocalDateTime;
 @Service
 public class BalanceService {
     BalanceDAO balanceDAO;
+    AccountDAO accountDAO;
 
     public Balance findBalanceByDateTime(LocalDateTime dateTime) {
         return balanceDAO.findByDateTime(dateTime);
+    }
+    public Balance findCurrentBalance(int accountId) {
+        return balanceDAO.findById(
+                accountDAO.findById(accountId).getBalanceId()
+        );
     }
 }
