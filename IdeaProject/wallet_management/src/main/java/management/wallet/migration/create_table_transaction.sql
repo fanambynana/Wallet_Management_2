6a63631ac128e7fe5dc40c1fce4ea35d8c3bd803
Faniry <faniryrandria08@gmail.com>
2023-12-14 17:21:32 +0300

    feat: add category column

--- IdeaProject/wallet_management/src/main/java/management/wallet/migration/create_table_transaction.sql
+++ IdeaProject/wallet_management/src/main/java/management/wallet/migration/create_table_transaction.sql
@@ -6,5 +6,6 @@ CREATE TABLE IF NOT EXISTS "transaction"(
     amount DECIMAL NOT NULL ,
     transaction_date TIMESTAMP NOT NULL DEFAUL CURRENT_TIMESTAMP,
     transaction_type TRANSACTION_TYPE NOT NULL,
+    categoryId INT REFERENCES transaction_categories(id),   --   this column specify the transaction category
     account_id INT NOT NULL REFERENCES account(id)
 );
