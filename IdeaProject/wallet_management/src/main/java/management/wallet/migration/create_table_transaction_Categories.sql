3f0d02bc709ce1e24fabbbbcbe8ea3cdfd08c293
Faniry <faniryrandria08@gmail.com>
2023-12-14 22:28:21 +0300

    feat: create table for transaction categories

--- IdeaProject/wallet_management/src/main/java/management/wallet/migration/create_table_transaction_Categories.sql
+++ IdeaProject/wallet_management/src/main/java/management/wallet/migration/create_table_transaction_Categories.sql
@@ -1,6 +1,5 @@
-CREATE TYPE SPECIFIC_CATEGORY AS ENUM ('expense', 'income', 'other')
-
-CREATE TABLE IF NOT EXIST transaction_Categories (
+CREATE TYPE SPECIFIC_CATEGORY AS ENUM ('expense', 'income', 'other');
+CREATE TABLE IF NOT EXISTS transaction_categories (
     Id SERIAL PRIMARY KEY,
     Category_name VARCHAR(50) NOT NULL,
     Category_categorie SPECIFIC_CATEGORY NOT NULL
