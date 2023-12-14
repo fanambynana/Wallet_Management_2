636edb797a7ee5141a2b2fbcc0256ef5e51ee28c
Faniry <faniryrandria08@gmail.com>
2023-12-14 17:20:34 +0300

    feat: create transactionCategories class

--- IdeaProject/wallet_management/src/main/java/management/wallet/DAO/TransactionCategoriesDAO.java
+++ IdeaProject/wallet_management/src/main/java/management/wallet/DAO/TransactionCategoriesDAO.java
@@ -1,5 +1,14 @@
 package management.wallet.DAO;
 
+import management.wallet.dbConnection.DbConnect;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+
+@Repository
 public class TransactionCategoriesDAO {
+    DbConnect connect = new DbConnect();
+    Connection connection = connect.createConnection();
+
 
 }
