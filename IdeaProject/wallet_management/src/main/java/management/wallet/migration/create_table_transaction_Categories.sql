c03375c3c6b026e9d065f7c5bf3e9cd6748f4f7d
fanambynana <hei.fanambinana@gmail.com>
2023-12-15 03:27:32 +0300

    fix: make column category name unique

--- IdeaProject/wallet_management/src/main/java/management/wallet/migration/create_table_transaction_Categories.sql
+++ IdeaProject/wallet_management/src/main/java/management/wallet/migration/create_table_transaction_Categories.sql
@@ -1,6 +1,6 @@
 CREATE TYPE SPECIFIC_CATEGORY AS ENUM ('expense', 'income', 'other');
 CREATE TABLE IF NOT EXISTS transaction_categories (
-    Id SERIAL PRIMARY KEY,
-    Category_name VARCHAR(50) NOT NULL,
-    Category_categorie SPECIFIC_CATEGORY NOT NULL
+    id SERIAL PRIMARY KEY,
+    category_name VARCHAR(50) UNIQUE NOT NULL,
+    category_categorie SPECIFIC_CATEGORY NOT NULL
 );
