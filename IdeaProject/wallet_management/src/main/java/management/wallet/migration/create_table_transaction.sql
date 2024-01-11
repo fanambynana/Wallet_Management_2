48ff50df53bceef3a73c28d9a294863e9fff9d3a
fanambynana <hei.fanambinana@gmail.com>
2024-01-11 06:24:56 +0300

    fix: modify the script syntax

--- IdeaProject/wallet_management/src/main/java/management/wallet/migration/create_table_transaction.sql
+++ IdeaProject/wallet_management/src/main/java/management/wallet/migration/create_table_transaction.sql
@@ -1,11 +1,11 @@
 CREATE TYPE TRANSACTION_TYPE AS ENUM('debit', 'credit');
 
-CREATE TABLE IF NOT EXISTS "transaction"(
+CREATE TABLE IF NOT EXISTS transaction(
     id SERIAL PRIMARY KEY ,
     label VARCHAR(50) NOT NULL,
     amount DECIMAL NOT NULL ,
-    transaction_date TIMESTAMP NOT NULL DEFAUL CURRENT_TIMESTAMP,
+    transaction_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     transaction_type TRANSACTION_TYPE NOT NULL,
-    category_id INT REFERENCES transaction_categories(id),   --   this column specify the transaction category
+    category_id INT REFERENCES transaction_categories(id),
     account_id INT NOT NULL REFERENCES account(id)
 );
--- IdeaProject/wallet_management/src/main/java/management/wallet/migration/insert_into_transaction.sql
+++ IdeaProject/wallet_management/src/main/java/management/wallet/migration/insert_into_transaction.sql
@@ -1,9 +1,9 @@
-INSERT INTO "transaction" (label, amount, transaction_date, transaction_type)
+INSERT INTO transaction (label, amount, transaction_date, transaction_type, category_id, account_id)
 VALUES
-('Salary', 100000, 'credit', '2023-12-01 12:15'),
-('Noël gift', 50000, 'debit', '2023-12-02 2:00'),
-('New shoe', 20000, 'debit', '2023-12-06 4:00'),
+('Salary', 100000, '2023-12-01 12:15', 'credit', 3, 1),
+('Noël gift', 50000, '2023-12-02 2:00', 'debit', 4, 2),
+('New shoe', 20000, '2023-12-06 4:00', 'debit', 5, 3),
 
-('Groceries', 50.00, CURRENT_TIMESTAMP, 'debit'),
-('Salary', 3000.00, '2023-12-01 09:00:00', 'credit'),
-('Electricity Bill', 80.00, '2023-11-20 15:30:00', 'debit');
+('Groceries', 50.00, CURRENT_TIMESTAMP, 'debit', 5, 3),
+('Salary', 3000.00, '2023-12-01 09:00:00', 'credit', 3, 2),
+('Electricity Bill', 80.00, '2023-11-20 15:30:00', 'debit', 6, 3);
